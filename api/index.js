49dc8855a2d8237993a2f4af28446654b301ae21
rola-houhou <rolahouhou85@gmail.com>
2024-11-15 09:55:46 +0100

    create test api routes

--- api/index.js
+++ api/index.js
@@ -16,11 +16,10 @@ mongoose
   });
 
 const app = express();
-app.use(cors());
 app.use(express.json());
-
-app.use('/api/products', productRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!!!");
 });
+
+app.use('/api/products', productRoutes);
